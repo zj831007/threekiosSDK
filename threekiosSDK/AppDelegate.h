3f9e7aa43178d7a1d490c648c03c4424a282651f
zj831007 <zj831007@163.com>
2013-01-26 13:52:06 +0800

    init project

---
+++ threekiosSDK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  threekiosSDK
+//
+//  Created by Justin on 13-1-26.
+//  Copyright (c) 2013年 3q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ threekiosSDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  threekiosSDK
+//
+//  Created by Justin on 13-1-26.
+//  Copyright (c) 2013年 3q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
