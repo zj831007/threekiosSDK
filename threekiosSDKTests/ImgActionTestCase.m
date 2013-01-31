a741d096bb3def95ad88e7878c891bb00716c97e
zj831007 <zj831007@163.com>
2013-01-31 23:26:24 +0800

    add test case for image upload

--- threekiosSDKTests/ImgActionTestCase.m
+++ threekiosSDKTests/ImgActionTestCase.m
@@ -7,7 +7,32 @@
 //
 
 #import "ImgActionTestCase.h"
+#import "ImageAction.h"
 
 @implementation ImgActionTestCase
+
+- (void)setUp{
+    [super setUp];
+    // Set-up code here.
+}
+
+- (void)tearDown{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+//图片上传
+- (void)testUpload{
+    ImageAction *imageAction = [ImageAction action];
+    imageAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"20003", @"图片不能为空");
+    };
+    
+    [imageAction upload:@1 accessToken:@"s" ext:@"3" type:@1 img:nil];
+    
+}
+
+
 
 @end
