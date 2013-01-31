b29803e56ea7a0d5ebba4d530da8b37ff2cef6a1
zj831007 <zj831007@163.com>
2013-01-31 23:16:42 +0800

    add test case to user

--- threekiosSDKTests/UserActionTestCase.m
+++ threekiosSDKTests/UserActionTestCase.m
@@ -22,6 +22,9 @@
     [super tearDown];
 }
 
+#pragma mark -
+#pragma mark Reg test case
+
 //用户名重复注册
 - (void)testReg{
     UserAction *userAction = [UserAction action];
@@ -35,18 +38,75 @@
 
 //敏感词注册
 - (void)testReg1{
-    
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10007", @"用户名包含敏感词");
+    };
+    [userAction reg:@"法轮功" password:@"123456"];
 }
 
 //手机号注册
 - (void)testReg2{
-    
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10008", @"用户名不能为手机号");
+    };
+    [userAction reg:@"13716981343" password:@"123456"];
 }
 
+#pragma mark -
+#pragma mark Login test case
 
+//登陆
 - (void)testLogin{
-    //STFail(@"U2222");
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10001", @"用户名或密码不正确");
+    };
+    [userAction login:@"111111" password:@"11111111"];
 }
 
+#pragma mark -
+#pragma mark Logout test case
+- (void)testLogout{
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10012", @"用户ID不存在");
+    };
+    [userAction logout:@0 accessToken:@"8888"];
+}
+
+- (void)testLogout1{
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10005", @"用户登陆信息已失效");
+    };
+    [userAction logout:@6 accessToken:@"8888"];
+}
+
+#pragma mark -
+#pragma mark Status test case
+
+
+#pragma mark -
+#pragma mark Profile test case
+- (void)testProfile{
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10012", @"用户ID不存在");
+    };
+    [userAction getProfile:@0];
+}
+
+#pragma mark -
+#pragma mark Profile test case
+- (void)testEditProfile{
+    
+    UserAction *userAction = [UserAction action];
+    userAction.resultErrorBlock = ^(id result){
+        STAssertEqualObjects([result objectForKey:@"error_code"], @"10007", @"用户ID不存在");
+    };
+    [userAction editProfile:[@{@"nickname":@"法轮功"} mutableCopy]];
+}
 
 @end
